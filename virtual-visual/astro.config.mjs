e2fc3e217f75f75b2c68c3c16be2d95fa4329f97
Marco Mendez <u202018273@upc.edu.pe>
2025-07-05 23:36:26 -0500

    fix: Corregir valores de site y base en astro.config

--- virtual-visual/astro.config.mjs
+++ virtual-visual/astro.config.mjs
@@ -1,12 +1,14 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
-  // Confirma que tu usuario de GitHub es 'cuak123' (sensible a mayúsculas)
   site: 'https://cuak123.github.io', 
-
-  // Confirma que el nombre del repositorio es EXACTAMENTE este
   base: '/LandingPage---Mosso',
-
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  
 });
